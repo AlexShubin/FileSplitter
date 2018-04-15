3f01496e8a5a9b0e91ad1e7b76ac05f9753faa70
Alex Shubin <alexshubin22@gmail.com>
2018-04-15 10:04:31 +0300

    Initial Commit

---
+++ FileSplitter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileSplitter
+//
+//  Created by Alex Shubin on 15/04/2018.
+//  Copyright © 2018 Alex Shubin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileSplitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileSplitter
+//
+//  Created by Alex Shubin on 15/04/2018.
+//  Copyright © 2018 Alex Shubin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
